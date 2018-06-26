ea7fde8b064b2b1fe79695315e76de1d9cba4755
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-26 01:09:07 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
 INSERT INTO bears (name) VALUES ("Mr. Chocolate")
+INSERT INTO bears (name) VALUES ("Rowdy")
+INSERT INTO bears (name) VALUES ("Tabitha")
+INSERT INTO bears (name) VALUES ("Mr. Chocolate")
+INSERT INTO bears (name) VALUES ("Mr. Chocolate")
+INSERT INTO bears (name) VALUES ("Mr. Chocolate")
+INSERT INTO bears (name) VALUES ("Mr. Chocolate")
+INSERT INTO bears (name) VALUES ("Mr. Chocolate")
